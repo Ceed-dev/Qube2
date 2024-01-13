e1bbe027c9691d067df079b012d8c05ce5b237fe
shungo0222 <kimura.shungo@gmail.com>
2024-01-13 13:59:39 +0900

    Fix: TypeError: Cannot read properties of undefined (reading 'PureComponent')
    Related to #214

--- frontend/pages/createProject.tsx
+++ frontend/pages/createProject.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import type { NextPage } from "next";
+// import React from "react";
+// import type { NextPage } from "next";
 
-import { ProjectScaffold, Notification } from "../components";
+// import { ProjectScaffold, Notification } from "../components";
 
-const CreateProject: NextPage = () => {
-  return (
-    <div>
-      <Notification />
-      <ProjectScaffold />
-    </div>
-  );
-};
+// const CreateProject: NextPage = () => {
+//   return (
+//     <div>
+//       <Notification />
+//       <ProjectScaffold />
+//     </div>
+//   );
+// };
 
-export default CreateProject;
+// export default CreateProject;
