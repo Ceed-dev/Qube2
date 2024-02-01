fc824bb28984d3fcdb694a6915072ed92115d9a3
shungo0222 <kimura.shungo@gmail.com>
2024-02-01 18:28:55 +0900

    Fix the footer of HP
    Related to #504

--- frontend/components/hp/Footer.tsx
+++ frontend/components/hp/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 // Image Imports
-import { TwitterIcon, MediumIcon, Discord, Game } from "../../assets";
+import { TwitterIcon, MediumIcon, X, QubeForFooter } from "../../assets";
 
 // Content Imports
 import { footerLinks } from "../../constants";
@@ -48,68 +48,72 @@ const FooterLegal = (): JSX.Element => {
 
 const Footer = () => {
   return (
-    <div className="bg-gray-900">
+    <div className="bg-white">
       <div className="flex lg:flex-row flex-col pt-10 px-20 lg:gap-0 gap-10">
-        <Image
-          src={Game}
-          alt="Game"
-          className="lg:h-[200px] mx-auto flex-1 p-5"
-        />
-        <div className="w-1 h-[200px] bg-gradient-to-b from-transparent via-[#E220CF] to-transparent lg:block hidden"></div>
-        <div className="flex-1 flex flex-col items-center">
-          <p className="text-2xl font-bold mb-3">RESOURCES</p>
-          <ul className="list-disc list-inside">
-            <li className="text-xl hover:underline">
-              <Link href="https://github.com/Succery-dev/Qube" target="_blank">
-                Github
-              </Link>
-            </li>
-            <li className="text-xl hover:underline">
-              <Link href="https://qube-1.gitbook.io/qube-whitepaper-japanese/" target="_blank">
-                Whitepaper Jap
-              </Link>
-            </li>
-            <li className="text-xl hover:underline">
-              <Link href="https://qube-1.gitbook.io/qube-whitepaper/" target="_blank">
-                Whitepaper Eng
-              </Link>
-            </li>
-            <li className="text-xl hover:underline">
-              <Link href="https://immunefi.com/" target="_blank">
-                Bug Bounty
-              </Link>
-            </li>
-          </ul>
+
+        <div className="flex-1 flex justify-center items-start">
+          <Image
+            src={QubeForFooter}
+            alt="Qube"
+            width={150}
+            height={150}
+          />
         </div>
-        <div className="w-1 h-[200px] bg-gradient-to-b from-transparent via-[#E220CF] to-transparent lg:block hidden"></div>
-        <div className="flex-1 flex flex-col items-center">
-          <p className="text-2xl font-bold mb-3">CONTACT</p>
-          <ul className="list-disc list-inside">
-            <li className="text-xl hover:underline">
-              <Link href="mailto:official@0xqube.xyz" target="_blank">
-                Mail Address
-              </Link>
-            </li>
-          </ul>
+
+        <div className="w-1 h-[200px] bg-gradient-to-b from-transparent via-purple-400 to-transparent lg:block hidden"></div>
+
+        <div className="flex-1 flex justify-center">
+          <div className="flex flex-col">
+            <p className="text-2xl font-bold mb-5">RESOURCES</p>
+            <ul>
+              <li className="text-xl hover:underline mb-5">
+                <Link href="https://github.com/Succery-dev/Qube" target="_blank">
+                  Qube Agent
+                </Link>
+              </li>
+              <li className="text-xl hover:underline">
+                <Link href="https://qube-1.gitbook.io/qube-whitepaper-japanese/" target="_blank">
+                  企業情報
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="w-1 h-[200px] bg-gradient-to-b from-transparent via-[#E220CF] to-transparent lg:block hidden"></div>
+        
+        <div className="w-1 h-[200px] bg-gradient-to-b from-transparent via-purple-400 to-transparent lg:block hidden"></div>
+
+        <div className="flex-1 flex justify-center">
+          <div className="flex flex-col">
+            <p className="text-2xl font-bold mb-5">CONTACT</p>
+            <ul>
+              <li className="text-xl hover:underline">
+                <Link href="https://docs.google.com/forms/d/e/1FAIpQLScds_7cNpaP777tQf910xgbd_ciFfZC9likpocEDzkPonWBrw/viewform" target="_blank">
+                  お問い合わせ
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        
+        <div className="w-1 h-[200px] bg-gradient-to-b from-transparent via-purple-400 to-transparent lg:block hidden"></div>
+
         <div className="flex-1 flex flex-col items-center">
           <p className="text-2xl font-bold mb-3">SOCIAL ACCOUNTS</p>
           <div className="flex flex-row gap-5">
             <Link href="https://twitter.com/0xQube" target="_blank">
-              <Image src={TwitterIcon} alt="Twitter" height={30} className="bg-[#613D5D] hover:bg-[#E220CF] ease-in duration-300 rounded-full h-[50px] w-[50px] p-1 border border-[#E220CF]" />
-            </Link>
-            <Link href={`${process.env.NEXT_PUBLIC_DISCORD_LINK}`} target="_blank">
-              <Image src={Discord} alt="Discord" height={30} className="bg-[#613D5D] hover:bg-[#E220CF] ease-in duration-300 rounded-full h-[50px] w-[50px] p-1 border border-[#E220CF]" />
+              <Image src={X} alt="X" height={50} width={50} />
             </Link>
             <Link href="https://medium.com/@0xqube" target="_blank">
-              <Image src={MediumIcon} alt="Medium" height={30} className="bg-[#613D5D] hover:bg-[#E220CF] ease-in duration-300 rounded-full h-[50px] w-[50px] p-1 border border-[#E220CF]" />
+              <Image src={MediumIcon} alt="Medium" height={50} width={50} />
             </Link>
           </div>
         </div>
+
       </div>
-      <hr className="w-2/3 h-[1px] bg-gradient-to-r from-transparent via-[#E220CF] to-transparent border-none mx-auto my-16" />
-      <p className="text-center pb-16 hover:underline">
+
+      <hr className="w-2/3 h-[4px] bg-gradient-to-r from-transparent via-purple-400 to-transparent border-none mx-auto my-16" />
+
+      <p className="font-bold text-center pb-16 hover:underline bg-gradient-to-t from-purple-200 to-transparent">
         <Link href="/">
           2023 © SUCCERY FZCO - ALL RIGHTS RESERVED
         </Link>
