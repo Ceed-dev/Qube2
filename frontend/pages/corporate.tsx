653531ff59c492cb80facc23b0350f5777ab7548
shungo0222 <kimura.shungo@gmail.com>
2024-02-02 10:43:06 +0900

    Create the products section on the corporate page
    Related to #522

--- frontend/pages/corporate.tsx
+++ frontend/pages/corporate.tsx
@@ -6,8 +6,9 @@ import { Footer } from "../components";
 import { useAccount, useDisconnect } from "wagmi";
 import { useRouter } from "next/router";
 import { useNotificationContext } from "../context";
-import { CorporateProfile } from "../assets";
+import { CorporateProfile, ArrowIconForFaq } from "../assets";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Corporate() {
   const router = useRouter();
@@ -30,6 +31,32 @@ export default function Corporate() {
       <div className="h-screen flex flex-col justify-center items-center font-bold">
         <p className="text-2xl mb-3">Our Mission</p>
         <p className="text-4xl">Mission系をデカデカと書く</p>
+      </div>
+
+      <div className="w-8/12 mx-auto my-32">
+        <h1 className="text-4xl font-bold font-sans mb-20">Products</h1>
+        <div className="flex flex-row gap-40">
+          <div>
+            <Link href="/" target="_blank">
+              <h2 className="flex flex-row text-3xl font-bold justify-between hover:text-slate-500 hover:cursor-pointer">
+                Qube
+                <Image src={ArrowIconForFaq} alt="arrow" />
+              </h2>
+            </Link>
+            <div className="h-[5px] bg-purple-500 my-3"></div>
+            <p>インフルエンサーマーケでの検索・契約・マネジメント・支払いまでの全てを一元化できるツールを提供しています。</p>
+          </div>
+          <div>
+            <Link href="/agent" target="_blank">
+              <h2 className="flex flex-row text-3xl font-bold justify-between hover:text-slate-500 hover:cursor-pointer">
+                Qube Agent
+                <Image src={ArrowIconForFaq} alt="arrow" />
+              </h2>
+            </Link>
+            <div className="h-[5px] bg-purple-500 my-3"></div>
+            <p>インド・フィリピン・インドネシアで、ブロックチェーン関連事業のマーケティングソリューションを提供するサービスです。</p>
+          </div>
+        </div>
       </div>
 
       <Image src={CorporateProfile} alt="CorporateProfile" className="mx-auto my-40" />
